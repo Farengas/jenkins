202c3a2cb4574de3159ab38992742cbc13f8d66a
Davide <davidereale12@gmail.com>
2024-09-02 14:46:19 +0200

    Initial

--- src/test/java/com/jenkins/jenkins/JenkinsApplicationTests.java
+++ src/test/java/com/jenkins/jenkins/JenkinsApplicationTests.java
@@ -14,6 +14,7 @@ class JenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing...");
+		logger.info("Secondo Test case executing...");
 		assertEquals(true, true);
 	}
 
